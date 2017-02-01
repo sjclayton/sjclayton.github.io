72085e3add3e9bec80fbf42750299b72763c8c8b
Shaun Clayton <shaun.j.clayton@gmail.com>
2017-02-01 10:12:35 -0500

    Mobile nav collapse on click

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,3 +2,11 @@ $(".nav li").on("click", function() {
       $(".nav li").removeClass("active");
       $(this).addClass("active");
 });
+
+$(function(){ 
+     var navMain = $(".navbar-collapse");
+
+     navMain.on("click", "a", null, function () {
+         navMain.collapse('hide');
+     });
+ });
