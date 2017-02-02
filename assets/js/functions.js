446149128cab15c9563016fe31f0a274ccaa1f62
Shaun Clayton <shaun.j.clayton@gmail.com>
2017-02-01 19:34:40 -0500

    Integrated smooth-scroll

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,3 +5,16 @@ $(function(){
          navMobile.collapse('hide');
      });
 });
+
+$(".navbar-smooth li a[href^='#']").on('click', function(event) {
+  var target;
+  target = this.hash;
+
+  event.preventDefault();
+
+  return $('html, body').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 500, function() {
+    return window.history.pushState(null, null, target);
+  });
+});
