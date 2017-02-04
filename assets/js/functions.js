58a885f3f997cce2c2385e9483501ca311de61f2
Shaun Clayton <shaun.j.clayton@gmail.com>
2017-02-03 21:10:12 -0500

    JS improvements / tooltips / hover effects

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,12 @@
 $(function(){
+     //Close mobile nav on link click
      var navMobile = $(".navbar-collapse");
 
      navMobile.on("click", "a", null, function () {
          navMobile.collapse('hide');
      })
 
+     //Sequentially fade in portfolio images on scroll
      var waypoint = $('#portfolio').waypoint(function(direction) {
        $('#fds img').each(function(i) {
          $(this).delay((i++) * 300).fadeTo(1000, 1); });
@@ -12,8 +14,11 @@ $(function(){
        offset: '50%'
      })
 
+     //Tooltips for link titles
+     $('a').tooltip({placement: 'top'})
 });
 
+//Smooth scrolling
 $(".navbar-smooth li a[href^='#']").on('click', function(event) {
   var target;
   target = this.hash;
