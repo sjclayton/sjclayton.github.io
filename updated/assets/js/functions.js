781cc58a86fc3e8283e9253b484bbfb0cacc8ba0
Shaun Clayton <shaun.j.clayton@gmail.com>
2017-02-05 10:50:48 -0500

    Added updated stub

---
+++ updated/assets/js/functions.js
@@ -0,0 +1,33 @@
+$(function(){
+     //Close mobile nav on link click
+     var navMobile = $(".navbar-collapse");
+
+     navMobile.on("click", "a", null, function () {
+         navMobile.collapse('hide');
+     })
+
+     //Sequentially fade in portfolio images on scroll
+     var waypoint = $('#portfolio').waypoint(function(direction) {
+       $('#fds img').each(function(i) {
+         $(this).delay((i++) * 300).fadeTo(1000, 1); });
+     }, {
+       offset: '50%'
+     })
+
+     //Tooltips for link titles
+     $('a').tooltip({placement: 'top', trigger: 'hover'})
+});
+
+//Smooth scrolling
+$(".navbar-smooth li a[href^='#']").on('click', function(event) {
+  var target;
+  target = this.hash;
+
+  event.preventDefault();
+
+  return $('html, body').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 700, function() {
+    return window.history.pushState(null, null, target);
+  });
+});
